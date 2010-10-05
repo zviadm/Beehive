dbb2617b90e1327662d70f787bf3ed32726714c6
Zviad Metreveli <zvio@mit.edu>
2010-10-05 17:20:39 -0400

    fixup bcast simulation code, add sequential tsp code

--- sw/test/bcastsim.c
+++ sw/test/bcastsim.c
@@ -14,21 +14,27 @@ void mc_init(void)
   printf("[%02u]: mc_init\n", corenum());
 }
 
-void mc_main(void) 
+__noret__ void mc_main(void) 
 {
   xprintf("[%02u]: mc_main\n", corenum());
   
   // Small Broadcast test
+  IntercoreMessage msg;    
+  unsigned int st;
+  const unsigned int x = 13;
+  
   if (corenum() == 2) {
-    unsigned int x = 13;
     xprintf("[%02u]: bcast x: %u t: %u\n", corenum(), x, msgTypeDefault);
     hw_bcast_send(msgTypeDefault, 1, &x);
-    //IntercoreMessage msg;
-    //msg[0] = 13;
-    //Message_send(3, msgTypeDefault, &msg, 1);
   } else {
-    IntercoreMessage msg;    
-    unsigned int st;
+    xprintf("[%02u]: send dest: %u x: %u t: %u\n", 
+      corenum(), 2, x, msgTypeDefault);
+    msg[0] = x;
+    message_send(2, msgTypeDefault, &msg, 1);
+  }
+
+  // All cores just say what messages they receive
+  while (1) {
     while ((st = message_recv(&msg)) == 0) { 
       icSleep(100);
     }
---
+++ sw/test/tsp/seqtsp.c
@@ -0,0 +1,95 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "shared/intercore.h"
+#include "lib/lib.h"
+
+#include "12_cities.h"
+
+void mc_init(void);
+void mc_main(void);
+
+void tsp(int len, int cost, int path[], int visited[], 
+         int best_path[], int *min);
+int present(int e, int len, int path[]);
+
+void mc_init(void) 
+{
+  printf("[%02u]: mc_init\n", corenum());
+}
+
+void mc_main(void) 
+{
+  xprintf("[%02u]: mc_main\n", corenum());
+  
+  if (corenum() == 2) {
+    int path[NRTOWNS];     // current (partial) tour path
+    int visited[NRTOWNS];  // count of tours of particular length
+    int best_path[NRTOWNS];// current best tour path
+    int min;               // cost of best tour path
+
+    // initialization
+    min = 10000;
+    for (unsigned int i = 0; i < NRTOWNS; i++) visited[i] = 0;
+    path[0] = 0;  // starting town, TSP is cycle so just choose town 0
+
+    printf("Starting TSP ...n");
+        
+    const unsigned int start_cycle = *cycleCounter;
+    tsp(1, 0, path, visited, best_path, &min); // find a min cost tour
+    const unsigned int end_cycle = *cycleCounter;
+
+    // print results
+    printf("shortest path length is %d\n", min);
+    printf("computation time (in CPU cycles): %d\n", end_cycle - start_cycle);
+
+    printf("level\tvisited\n");
+    for (unsigned int i = 0; i < NRTOWNS; i++) printf("%d\t%d\n",i,visited[i]);
+
+    printf("a best path found: ");
+    for (unsigned int i = 0; i < NRTOWNS; i++) printf("%d ", best_path[i]);
+    printf("\n");  
+  }
+}
+
+// recursive TSP search: look for a town to visit, path[] contains
+// the len towns visited so far with a total (partial)
+// tour length of cost.
+void tsp(int len, int cost, int path[], int visited[], 
+         int best_path[], int *min)
+{
+  const int me = path[len-1];	// current end town
+  // remember how many times we saw a tour of this length
+  visited[len-1]++;
+
+  if (len == NRTOWNS) {
+    const int new_cost = cost + distance[me][path[0]];
+    if (new_cost < *min) {
+      // new min cost tour, remember cost and path
+      *min = new_cost;
+      for (unsigned int i = 0; i < NRTOWNS; i++) best_path[i] = path[i];
+    }
+  } else {
+    // look for next town in tour
+    for (unsigned int i = 0; i < NRTOWNS; i++) {
+      const int new_cost = cost + distance[me][i];
+      if (!present(i, len, path) & (new_cost < *min)) {
+        path[len] = i;
+        tsp(len + 1, new_cost, path, visited, best_path, min);
+      }
+    }
+  }
+}
+
+// useful helper function:
+// is virtex e present in first len entries of path?
+int present(int e, int len, int path[]) 
+{
+  int i;
+
+  for (i = 0; i < len; i++)
+    if (path[i] == e)
+      return 1;
+  return 0;
+}
