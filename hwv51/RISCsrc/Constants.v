ae563f52d876b8922bfdc8ca1e7ae1ef89eb03ee
Zviad Metreveli <zvio@mit.edu>
2010-10-16 13:20:10 -0400

    add constants file

---
+++ hwv51/RISCsrc/Constants.v
@@ -0,0 +1,12 @@
+// Define SlotTypes
+`define StartUp        4'd0
+`define Token          4'd1
+`define Address        4'd2
+`define WriteData      4'd3
+`define GrantExclusive 4'd6
+`define Null           4'd7
+`define Message        4'd8
+`define Preq           4'd9
+`define Pfail          4'd10
+`define Vreq           4'd11
+`define Barrier        4'd12
